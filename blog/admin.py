10b22306440a89a18dd0305e09e96e0c146691c8
Manish Bhusal <manishbhusal6@gmail.com>
2023-05-20 14:00:18 +0545

    Complete adding Data and Models in django app

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Author, Post, Tag
 
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields={"slug": ("title",)}
+    list_filter=("author", "tags", "date")
+    list_display=("title", "date", "author")
+
+
+admin.site.register(Author)
+admin.site.register(Post,PostAdmin)
+admin.site.register(Tag)
